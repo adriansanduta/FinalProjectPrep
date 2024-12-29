95e3be8fc95d89da302622f3c1ba31d757f12a6b
Adrian <adriansanduta@gmail.com>
2024-12-29 16:30:20 -0600

    1.2

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ async function main() {
     const usersData = await users.json();
     const userListEl = document.querySelector('.user-list');
 
-    userListEl.innerHTML = usersData
-    .map((user) => `<div class="user-card">
-        <div class="user-card__container">
-            <h3>${user.name}</h4>
-            <p><b>Email:</b> ${user.email}</p>
-            <p><b>Phone:</b> ${user.phone}</p>
-            <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
-        </div>
-    </div>`)
-    .join("");
+    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
 }
 
 main();
+
+function userHTML(user) {
+    return `<div class="user-card">
+            <div class="user-card__container">
+              <h3>${user.name}</h4>
+                <p><b>Email:</b> ${user.email}</p>
+                <p><b>Phone:</b> ${user.phone}</p>
+                <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+            </div>
+          </div>`;
+}

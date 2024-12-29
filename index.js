13d12c588cd5b08f358951d43dd74a2c82664af0
Adrian <adriansanduta@gmail.com>
2024-12-29 16:25:56 -0600

    1.1

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 
 async function main() {
     const users = await fetch("https://jsonplaceholder.typicode.com/users")
-    const usersData = users.json();
+    const usersData = await users.json();
     const userListEl = document.querySelector('.user-list');
-    usersData.map(user => `<div class="user-card">
+
+    userListEl.innerHTML = usersData
+    .map((user) => `<div class="user-card">
         <div class="user-card__container">
-            <h3>User's Name</h4>
-            <p><b>Email:</b> email@email.com</p>
-            <p><b>Phone:</b> 0000000000</p>
-            <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+            <h3>${user.name}</h4>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
         </div>
     </div>`)
     .join("");
